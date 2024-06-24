831ad9ee96a190b1b8add001401af11c9c4c0019
dianeib <59963593+dianeib@users.noreply.github.com>
2024-06-24 16:04:28 +0000

    7
    jk

--- resources/views/mail/rendezvous/rdv-valide.blade.php
+++ resources/views/mail/rendezvous/rdv-valide.blade.php
@@ -1,4 +1,4 @@
-<x-mail::message>
+{{-- <x-mail::message>
     Bonjour Mme/Mr, {{ $prenom }} {{ $nom }},
 
     votre Rendez-vous à été validé et votre code de confirmation est : {{ $code }}. Veillez gardez ce code.
@@ -6,4 +6,29 @@
     Date : {{ $date_rendezvous }} à {{ $heure_rendezvous }}
     Heure : {{ $heure_rendezvous }}
     Lieu : {{ $agence }}
-</x-mail::message>
+</x-mail::message> --}}
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Mail</title>
+</head>
+
+<body>
+    <p>
+        <img src="{{ $message->embed(public_path('new logo.jpeg')) }}" alt="here logo">
+    </p>
+    <h3>Bonjour Mme/Mr, {{ $prenom }} {{ $nom }},</h3>
+    <p>Votre Rendez-vous a bien été reçu à la Caisse Nationale de Sécurité Sociale (CNSS)</p>
+    <p>Le numero de votre code de confirmation est: <span
+            style="font-weight: bold; color: red;">{{ $code }}</span></p>
+    <p>Date : {{ $date_rendezvous }} .</p>
+    <p>Heure : {{ $heure_rendezvous }} .</p>
+    <p>Lieu : {{ $agence }} .</p>
+    <p>Si vous souhaitez faire une reclamation, veuillez <a
+            href="https://www.reclamations.cnssgn.com/reclamation/create">Cliquez ici</a></p>
+    <p>NB: Veuilliez ne pas repondre a ce mail.</p>
+
+</body>
+
+</html>
--- resources/views/mail/rendezvous/rendezvous.blade.php
+++ resources/views/mail/rendezvous/rendezvous.blade.php
@@ -8,3 +8,27 @@
 
 
 </x-mail::message>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Mail</title>
+</head>
+
+<body>
+    <p>
+        <img src="{{ $message->embed(public_path('new logo.jpeg')) }}" alt="here logo">
+    </p>
+    <h3>Bonjour Mme/Mr, {{ $prenom }} {{ $nom }},</h3>
+    <p>Votre Rendez-vous a bien été reçu à la Caisse Nationale de Sécurité Sociale (CNSS)</p>
+    <p>Le numero de votre code de confirmation est: <span
+            style="font-weight: bold; color: red;">{{ $code }}</span></p>
+    <p>Une e-mail de confirmation vous serez envoyé une fois que la demande sera traitée par notre équipe.</p>
+    <p>Si vous souhaitez faire une reclamation, veuillez <a
+            href="https://www.reclamations.cnssgn.com/reclamation/create">Cliquez ici</a></p>
+    <p>NB: Veuilliez ne pas repondre a ce mail.</p>
+
+</body>
+
+</html>
